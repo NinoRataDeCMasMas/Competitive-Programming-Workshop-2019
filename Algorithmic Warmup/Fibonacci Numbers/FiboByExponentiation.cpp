49cd1953d4da8d84f116de3a685955381c3890bc
ninoratadecmasmas <a310831@uach.mx>
2019-04-09 15:46:11 -0600

    Update the code to the C++ standard II R Please enter the commit message for your changes. Lines starting

--- Algorithmic Warmup/Fibonacci Numbers/FiboByExponentiation.cpp
+++ Algorithmic Warmup/Fibonacci Numbers/FiboByExponentiation.cpp
@@ -7,12 +7,13 @@
 
 using Matrix = std::array<std::array<uint64_t,2>,2>;
 /**
- * @brief  Matrix 2x2 product.
+ * @brief  Matrix 2x2 product algorithm.
  */
-Matrix product( Matrix A, Matrix B )
+Matrix operator*( Matrix& A, Matrix& B )
 {
     Matrix C;
-    C[0][0] = A[0][0]*B[0][0] + A[1][0]*B[0][1];  
+    // Schoolbook algorithm
+    C[0][0] = A[0][0]*B[0][0] + A[1][0]*B[0][1];
     C[0][1] = A[0][0]*B[1][0] + A[1][0]*B[1][1];
     C[1][0] = A[0][1]*B[0][0] + A[1][1]*B[0][1];
     C[1][1] = A[0][1]*B[1][0] + A[1][1]*B[1][1];
@@ -20,21 +21,21 @@ Matrix product( Matrix A, Matrix B )
     return C;
 }
 /**
- * @brief  Matrix 2x2 exponentiation.
+ * @brief  Matrix 2x2 exponentiation algorithm.
  */
 Matrix FiboByExp( Matrix A, uint64_t n )
 {
-    Matrix ans = {{{1, 0}, {0, 1}}}; // El calculo de nuestro resultado
+    Matrix ans = {{{1, 0}, {0, 1}}}; // Matriz identidad
 
     while(n > 0)
-    {
+    {   
         /*
          * Cada potencia impar multiplicamos al calculo el termino que nos 
          * hace falta. Cuando la potencia llega a ser 1, todo el calculo se
          * resguarda en ans
-         */        
-        if(n%2 == 1) ans = product(ans, A);
-        A = product(A, A); // Calculamos el cuadrado de A
+         */
+        if(n%2 == 1) ans = ans * A;
+        A = A * A;   // Calculamos el cuadrado de A
         /*
          * obtenemos la siguiente potencia de 2 a calcular. Las potencias
          * impares se obtienen automatica/e por el "casteo" de las variables
@@ -48,7 +49,7 @@ Matrix FiboByExp( Matrix A, uint64_t n )
  */
 int main( void )
 {
-    Matrix F = { {{1, 1}, {1, 0}} }; // Matriz de Fibonacci
+    Matrix F  = { {{1, 1}, {1, 0}} }; // Matriz de Fibonacci
     
     int64_t n = 0;
     std::cin >> n;
