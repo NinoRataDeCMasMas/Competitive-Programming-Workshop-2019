6a7f383e5816ceaa8762b00cde1b2628eaeb9856
ninoratadecmasmas <a310831@uach.mx>
2019-04-09 00:17:16 -0600

    Update exponentiation by squaring and adding Fibonacci by exponentiation

--- Algorithmic Warmup/Exponentiation/Exponentiation.cpp
+++ Algorithmic Warmup/Exponentiation/Exponentiation.cpp
@@ -1,34 +1,47 @@
 /**
  * @file   Exponentiation.cpp
- * @date   Jun 29, 2018
+ * @date   Abr 9, 2019
  * @brief  Code for the competitive programming workshop.
  */
 #include <bits/stdc++.h>
 /**
  * @brief  Recursive exponentiation Algorithm.
  */
-unsigned long exp( unsigned long x, unsigned long n )
+uint64_t exp( uint64_t x, uint64_t n )
 {
-    if(n == 0) return 1;
-    if(n == 1) return x;
+    /*
+     * Usamos esta condicion base porque usamos la expresion
+     * (n - 1)/2, que podria ser 0
+     */
+    if(n == 0) return 1;  
+    if(n == 1) return x; // Condicion base
 
-    if(n%2 == 1) return x*exp(x*x, (n - 1)/2);
-    else return exp(x*x, n/2);
+    if(n%2 == 1) return x*exp(x*x, (n - 1)/2); // Si la potencia es impar
+    return exp(x*x, n/2); // Si la potencia es par
 }
 /**
  * @brief  Iterative exponentiation Algorithm.
  */
-unsigned long itExp( unsigned long x, unsigned long n )
+uint64_t itExp( uint64_t x, uint64_t n )
 {
-    unsigned long ans = 1;
-
+    uint64_t ans = 1; // El calculo de nuestro resultado
+    
     while( n > 0 )
     {
-        if(n%2 == 1) ans = ans * x;
-        x = x*x;
-        n = n/2;
+        /*
+         * Cada potencia impar multiplicamos al calculo el termino que nos 
+         * hace falta. Cuando la potencia llega a ser 1, todo el calculo se
+         * resguarda en ans
+         */
+        if(n%2 == 1) ans *= x;
+        
+        x *= x; // Calculamos el cuadrado de x
+        /*
+         * obtenemos la siguiente potencia de 2 a calcular. Las potencias
+         * impares se obtienen automatica/e por el "casteo" de las variables
+         */
+        n  = n/2;
     }
-
     return ans;
 }
 /**
@@ -36,8 +49,9 @@ unsigned long itExp( unsigned long x, unsigned long n )
  */
 int main( void )
 {
-    unsigned long x, n;
+    uint64_t x, n;
     std::cin >> x >> n;
-    std::cout << exp(x, n) << std::endl;
+    std::cout << itExp(x, n) << std::endl;
     return 0;
 }
+/* EOF */
---
+++ Algorithmic Warmup/Fibonacci Numbers/FiboByExponentiation.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file   FiboByExponentiation.cpp
+ * @date   Abr 9, 2019
+ * @brief  Code for the competitive programming workshop.
+ */
+#include <bits/stdc++.h>
+
+using Matrix = std::array<std::array<uint64_t,2>,2>;
+/**
+ * @brief  Matrix 2x2 product.
+ */
+Matrix product( Matrix A, Matrix B )
+{
+    Matrix C;
+    C[0][0] = A[0][0]*B[0][0] + A[1][0]*B[0][1];  
+    C[0][1] = A[0][0]*B[1][0] + A[1][0]*B[1][1];
+    C[1][0] = A[0][1]*B[0][0] + A[1][1]*B[0][1];
+    C[1][1] = A[0][1]*B[1][0] + A[1][1]*B[1][1];
+
+    return C;
+}
+/**
+ * @brief  Matrix 2x2 exponentiation.
+ */
+Matrix FiboByExp( Matrix A, uint64_t n )
+{
+    Matrix ans = {{{1, 0}, {0, 1}}}; // El calculo de nuestro resultado
+
+    while(n > 0)
+    {
+        /*
+         * Cada potencia impar multiplicamos al calculo el termino que nos 
+         * hace falta. Cuando la potencia llega a ser 1, todo el calculo se
+         * resguarda en ans
+         */        
+        if(n%2 == 1) ans = product(ans, A);
+        A = product(A, A); // Calculamos el cuadrado de A
+        /*
+         * obtenemos la siguiente potencia de 2 a calcular. Las potencias
+         * impares se obtienen automatica/e por el "casteo" de las variables
+         */        
+        n = n/2;
+    }
+    return ans;
+}
+/**
+ * @brief  Main function.
+ */
+int main( void )
+{
+    Matrix F = { {{1, 1}, {1, 0}} }; // Matriz de Fibonacci
+    
+    int64_t n = 0;
+    std::cin >> n;
+    std::cout << FiboByExp(F, n)[0][1] << std::endl;
+    return 0;
+}
+/* EOF */
