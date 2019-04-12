476bc9bd84ab14c2f85ca0fb1d33a1cdf74c76d8
ninoratadecmasmas <a310831@uach.mx>
2019-04-12 01:20:11 -0600

    Update maxLinearSum algorithm.

--- Dynamic Programming/maxLinearSum.cpp
+++ Dynamic Programming/maxLinearSum.cpp
@@ -5,7 +5,7 @@
  */
 #include <bits/stdc++.h>
 
-int A[100][100] = {0};
+int A[100][100] = {0}, sum[101][101] = {0};
 /**
  * @brief Recurrence relation for path of maximal sum.
  */
@@ -17,6 +17,18 @@ int linearSum(int i, int j)
         return A[i][j] + std::max(linearSum(i, j - 1), linearSum(i - 1, j));     
 }
 /**
+ * @brief Tabulation method.
+ * @complexity is O(nm)
+ */
+int tabulation(int n, int m)
+{
+   for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= m; j++)
+            sum[i][j] = A[i - 1][j - 1] + std::max(sum[i][j - 1], sum[i - 1][j]);
+    
+    return sum[n][m];
+}
+/**
  * @brief Main function.
  */
 int main( void )
@@ -25,9 +37,8 @@ int main( void )
     std::cin >> n >> m;
     
     for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
-            std::cin >> A[i][j];
-        
-    std::cout << linearSum(n, m) << std::endl;    
+        for(int j = 0; j < m; j++) std::cin >> A[i][j];
+
+    std::cout << tabulation(n, m) << std::endl;    
     return 0;
 }
